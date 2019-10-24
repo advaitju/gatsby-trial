77720ba2a43c0e6617780108a9f3e8179548bb64
Advait Junnarkar <ajunnarkar@modernstar.com>
2019-10-24 15:20:40 +1100

    Updates

---
+++ component---src-pages-about-js-6639ef5174d99bf2e323.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{207:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a);t.default=function(){return c.a.createElement("div",{style:{color:"teal"}},c.a.createElement("h1",null,"About Gatsby"),c.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-6639ef5174d99bf2e323.js.map
